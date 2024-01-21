9518638e3e1154364d0780009b80f13c4e179763
kou1127h <pg5027ace@gmail.com>
2024-01-21 15:36:28 +0900

    背景色の修正など

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <p>
-          3時間一人ハッカソン
+          3時間ひとりハッカソン
         </p>
       </div>
     </main>
